e1540844b23084d274b7893457b0b1ca7899189b
Oyeniyi Elizabeth <oyeniyielizabeth123@gmail.com>
2025-02-17 10:50:21 -0800

    added color to code

--- obj/Debug/net9.0/LibraryApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/LibraryApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9db65d9622e08e7556763e5792501bf9377250")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
